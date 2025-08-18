4aff3d207888bbdbf0fea3a891b26d583a470e32
AshrafSada <Ashraf.Sada@outlook.com>
2025-08-18 18:32:03 +0300

    - updated folders

--- ZuziGame/clean.ps1
+++
@@ -1,9 +0,0 @@
-# clean.ps1
-
-$extensions = @("*.obj", "*.exe", "*.pdb", "*.ilk", "*.log")
-
-foreach ($ext in $extensions) {
-    Get-ChildItem -Path . -Filter $ext -Recurse | Remove-Item -Force
-}
-
-Write-Host "Clean complete. Removed: $($extensions -join ', ')"
